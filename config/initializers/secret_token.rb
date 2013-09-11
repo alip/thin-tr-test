8ddb68dc8378b11895963e4136e31518499c12dc
Ali Polatel <alip@exherbo.org>
2013-09-11 13:46:51 +0300

    sample spree site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinTrTest::Application.config.secret_token = 'eebe6412e35853b50488cca9f6358b3d1d3078aa276452dc7dcacc4995679392a60d1331263bef87e5bac4cb2708742c9cf917aec7fa1849b96caa904857e878'
